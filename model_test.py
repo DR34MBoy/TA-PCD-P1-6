f76c693221f056a243b998854e64abc3693eec57
DR34MBoy <fgh0608@gmail.com>
2023-05-22 14:03:57 +0700

    update

---
+++ model_test.py
@@ -0,0 +1,24 @@
+import cv2
+import matplotlib.pyplot as plt
+import numpy as np
+from model_train import test
+
+from model_train import rmodel
+
+#### TESTING ####
+img_path=r"images/Parasitized_(900).png"
+img=cv2.imread(img_path)  
+
+plt.imshow(img)  
+plt.show() 
+
+test(img)
+
+# resize_img = cv2.resize(img,(100,100))
+# array_image=np.array(resize_img)
+# array_image=array_image.reshape(1,30000)
+
+# print(rmodel.predict(array_image).reshape(1,-1))
+
+# plt.imshow(array_image.reshape(100,100,3)) 
+# plt.show()
--- model_train.py
+++ model_train.py
@@ -4,6 +4,7 @@ import matplotlib.pyplot as plt
 import os  
 from sklearn.model_selection import train_test_split
 from sklearn.ensemble import RandomForestClassifier
+from sklearn.metrics import accuracy_score, confusion_matrix, precision_score, recall_score, ConfusionMatrixDisplay
 
 ### TRAINING ###
 directory_path=r'images'
@@ -35,23 +36,15 @@ xtrain,xtest,ytrain,ytest=train_test_split(X,Y)
 rmodel=RandomForestClassifier()
 rmodel.fit(xtrain,ytrain)
 
-print(rmodel.score(xtrain,ytrain))
-print(rmodel.score(xtest,ytest))
+print("Train accuracy: ", rmodel.score(xtrain,ytrain))
+print("Test accuracy: ", rmodel.score(xtest,ytest))
 
+def test(img) :
+    resize_img = cv2.resize(img,(100,100))
+    array_image=np.array(resize_img)
+    array_image=array_image.reshape(1,30000)
 
+    print(rmodel.predict(array_image).reshape(1,-1))
 
-#### TESTING ####
-img_path=r"images/Uninfected_(10).png"
-img=cv2.imread(img_path)  
-
-plt.imshow(img)  
-plt.show() 
-
-resize_img = cv2.resize(img,(100,100))
-array_image=np.array(resize_img)
-array_image=array_image.reshape(1,30000)
-
-print(rmodel.predict(array_image).reshape(1,-1))
-
-plt.imshow(array_image.reshape(100,100,3)) 
-plt.show()
+    plt.imshow(array_image.reshape(100,100,3)) 
+    plt.show()
