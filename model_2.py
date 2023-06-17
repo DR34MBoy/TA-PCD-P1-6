a15a1fb4ea267f5f75b9cdee38562e476e9a134e
DR34MBoy <fgh0608@gmail.com>
2023-06-17 19:19:12 +0700

    update

--- model_2.py
+++ model_2.py
@@ -9,6 +9,9 @@ from sklearn.metrics import accuracy_score
 from sklearn.model_selection import StratifiedKFold
 from sklearn.ensemble import RandomForestClassifier
 from sklearn.cluster import KMeans
+from sklearn.feature_selection import SelectFromModel, RFECV
+import pandas as pd
+from skimage.feature import hog
 
 ### TRAINING ###
 directory_path = r'images_new'
@@ -31,7 +34,7 @@ X = np.array(features)
 Y = np.array(label)
 X = X.reshape(len(features), -1)
 
-x_train, x_test, y_train, y_test = train_test_split(X, Y, test_size=0.3, random_state=5)
+x_train, x_test, y_train, y_test = train_test_split(X, Y, test_size=0.3, random_state=42)
 
 
 # Random Forest Classifier tanpa tuning
@@ -52,16 +55,46 @@ x_train, x_test, y_train, y_test = train_test_split(X, Y, test_size=0.3, random_
 
 # Random Forest Classifier dengan tuning
 
-# parameters = {  'n_estimators':[50,60,70,80,90,100],
-#                 # 'min_samples_leaf':[1,2,3,4],
-#                 'max_depth':[10,20,30],
-#                 # 'max_features':[10,20,30,40,50,60,70,80,90,100],
-#                 # 'criterion':['gini','entropy']
-#             }
+parameters = {  'n_estimators':[50,60,70,80,90,100],
+                'min_samples_leaf':[1,2,3,4],
+                'max_depth':[10,20,30],
+                'max_features':[10,20,30,40,50],
+                'criterion':['gini','entropy']
+            }
 
-# rf = RandomForestClassifier(n_estimators=100, max_depth=50, min_samples_leaf=1, max_features=100)
-rf = RandomForestClassifier(n_estimators=100, max_depth=50)
-rf.fit(x_train, y_train)
+# rf =RandomForestClassifier(n_estimators=100, criterion='gini' ,max_depth=30,  min_samples_leaf=2, max_features=30)
+# feature_selector = SelectFromModel(rf)
+# feature_selector.fit(x_train, y_train)
+
+# x_train_selected = feature_selector.transform(x_train)
+# X_test_selected = feature_selector.transform(x_test)
+
+# rf.fit(x_train_selected, y_train)
+
+# y_pred_train = rf.predict(x_train_selected)
+#     # predictions for test
+# y_pred_test = rf.predict(X_test_selected)
+#     # training metrics
+# print("Training metrics:")
+# print(sklearn.metrics.classification_report(y_true= y_train, y_pred= y_pred_train))
+    
+#     # test data metrics
+# print("Test data metrics:")
+# print(sklearn.metrics.classification_report(y_true= y_test, y_pred= y_pred_test))
+
+rf = RandomForestClassifier()
+rf = GridSearchCV(rf, param_grid=parameters, scoring='accuracy')
+
+feature_selector = SelectFromModel(rf)
+feature_selector.fit(x_train, y_train)
+
+x_train_selected = feature_selector.transform(x_train)
+X_test_selected = feature_selector.transform(x_test)
+
+rf.fit(x_train_selected, y_train)
+
+best_params = rf.best_params_
+print("Best Parameters:", best_params)
 
 y_pred_train = rf.predict(x_train)
     # predictions for test
@@ -73,21 +106,6 @@ print(sklearn.metrics.classification_report(y_true= y_train, y_pred= y_pred_trai
     # test data metrics
 print("Test data metrics:")
 print(sklearn.metrics.classification_report(y_true= y_test, y_pred= y_pred_test))
-
-# rf = RandomForestClassifier()
-# clf = GridSearchCV(rf, param_grid=parameters)
-# model = clf.fit(x_train,y_train)
-
-# y_pred_train = model.predict(x_train)
-#     # predictions for test
-# y_pred_test = model.predict(x_test)
-#     # training metrics
-# print("Training metrics:")
-# print(sklearn.metrics.classification_report(y_true= y_train, y_pred= y_pred_train))
-    
-#     # test data metrics
-# print("Test data metrics:")
-# print(sklearn.metrics.classification_report(y_true= y_test, y_pred= y_pred_test))
 
 
 
