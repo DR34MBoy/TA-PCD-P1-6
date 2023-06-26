0b54520bf0774ea51e2a06a4fed692c1240261d5
DR34MBoy <fgh0608@gmail.com>
2023-06-26 15:01:33 +0700

    update

--- input_img.py
+++ input_img.py
@@ -1,15 +1,8 @@
-import cv2
-import matplotlib.pyplot as plt
-from model import test
+from model import classify_image
 import os
 
 img_path="images_test_run"
 
 test_img_dir = os.path.join(img_path)
 for image_file in os.listdir(test_img_dir):
-    if image_file.endswith('.png'):
-        image = cv2.imread(os.path.join(test_img_dir, image_file))
-        plt.imshow(image)  
-        plt.show() 
-        print(image_file)
-        test(image)
+    classify_image(os.path.join(test_img_dir, image_file))
--- model.py
+++ model.py
@@ -1,138 +1,138 @@
-import cv2
-import numpy as np
-import matplotlib.pyplot as plt
-import os
 import sklearn
+import os
+import cv2
+import mahotas
+
+import numpy as np
 import pandas as pd
-from sklearn.model_selection import train_test_split, GridSearchCV
-from sklearn.ensemble import RandomForestClassifier
-from sklearn.metrics import accuracy_score, confusion_matrix
-from sklearn.metrics import confusion_matrix
 import matplotlib.pyplot as plt
-import seaborn as sns
 
+from sklearn.svm import SVC
+from sklearn.model_selection import train_test_split
+from sklearn.preprocessing import StandardScaler
+from sklearn.metrics import accuracy_score, confusion_matrix, ConfusionMatrixDisplay
+from sklearn.ensemble import RandomForestClassifier
 
-### TRAINING ###
-# Array untuk features dan label
-features = []
-label = []
+directory_path = r'./images_folder'
+print(len(os.listdir(directory_path)))
 
-# Folder gambar
-image_folder_path = "./images_folder"
+files=[]
+for dirname, _, filenames in os.walk(directory_path):
+    for filename in filenames:
+        if '.db' not in filename:
+            files.append(os.path.join(dirname,filename))
 
-# Cek jumlah isi konten folder
-print(len(os.listdir(image_folder_path)))
+Parasitized_Dir='./images_folder/Parasitized'
+Uninfected_Dir='./images_folder/Uninfected'
 
-# Baca folder "./images_folder/Parasitized"
-parasitized_dir = os.path.join(image_folder_path, 'Parasitized')
-for image_file in os.listdir(parasitized_dir):
-    if image_file.endswith('.png'):
-        image = cv2.imread(os.path.join(parasitized_dir, image_file))
-        image = cv2.cvtColor(image, cv2.COLOR_BGR2RGB)  # Ubah ke format warna RGB
-        image = cv2.resize(image, (100, 100))
-        features.append(image)
-        label.append('Parasitized')
+pd.DataFrame(files).sample(frac=1).reset_index(drop=True)
 
-# Baca folder "./images_folder/Uninfected"
-uninfected_dir = os.path.join(image_folder_path, 'Uninfected')
-for image_file in os.listdir(uninfected_dir):
-    if image_file.endswith('.png'):
-        image = cv2.imread(os.path.join(uninfected_dir, image_file))
-        image = cv2.cvtColor(image, cv2.COLOR_BGR2RGB)  # Ubah ke format warna RGB
-        image = cv2.resize(image, (100, 100))
-        features.append(image)
-        label.append('Uninfected')
-
-# Buat jadi Array
-X = np.array(features)
-Y = np.array(label)
-X = X.reshape(len(features), -1)
-
-# Split Dataset
-x_train, x_test, y_train, y_test = train_test_split(X, Y, test_size=0.1, random_state=42)
-
-# Ubah dimensi data menjadi 1D (flatten)
-x_train = x_train.reshape(x_train.shape[0], -1)
-x_test = x_test.reshape(x_test.shape[0], -1)
-
-# Normalisasi nilai piksel menjadi range 0-1
-x_train = x_train / 255.0
-x_test = x_test / 255.0
-
-
-### Random Forest Classifier tanpa tuning ###
-
-# rf = RandomForestClassifier() 
-# rf.fit(x_train, y_train)
-
-# y_pred_train = rf.predict(x_train)
-# y_pred_test = rf.predict(x_test)
-
-# cm = confusion_matrix(y_test, y_pred_test)
-# plt.figure(figsize=(10, 8))
-# sns.heatmap(cm, annot=True, cmap='Blues', fmt='g', xticklabels=['Parasitized', 'Uninfected'], yticklabels=['Parasitized', 'Uninfected'])
-# plt.xlabel('Predicted')
-# plt.ylabel('Actual')
-# plt.title('Confusion Matrix')
-# plt.show()
-
-#     # training metrics
-# print("Training metrics:")
-# print(sklearn.metrics.classification_report(y_true= y_train, y_pred= y_pred_train))
+class DetectMalaria:
+    def __init__(self,para_dir,uninfect_dir):
+        self.parasitized_dir=para_dir
+        self.uninfected_dir=uninfect_dir
+    def dataset(self,ratio,files):
+        Dataset=pd.DataFrame(files,columns=['Path'])
+        Dataset=Dataset.sample(frac=1).reset_index(drop=True)
+        trainfiles,testfiles=train_test_split(Dataset,test_size=ratio,random_state=None)
+        return(trainfiles,testfiles)
     
-#     # test data metrics
-# print("Test data metrics:")
-# print(sklearn.metrics.classification_report(y_true= y_test, y_pred= y_pred_test))
+x=DetectMalaria(Parasitized_Dir,Uninfected_Dir)
+
+train_data,test_data=x.dataset(ratio=0.2,files=files)
+
+def label(df):
+    if 'Uninfected' in df:
+        return 0
+    else:
+        return 1
 
 
-### Random Forest Classifier dengan tuning ###
-# parameters = {  'n_estimators':[50,60,70,80,90,100],
-#                 'min_samples_leaf':[1,2,3,4],
-#                 'max_depth':[10,20,30],
-#                 'max_features':[10,20,30,40,50],
-#                 'criterion':['gini','entropy']
-#             }
+train_data['label']=train_data['Path'].apply(label)
+test_data['label']=test_data['Path'].apply(label)
 
-# rf = RandomForestClassifier()
-# rf = GridSearchCV(rf, param_grid=parameters, scoring='accuracy')
+def fd_hu_moments(image):
+    image = cv2.cvtColor(image, cv2.COLOR_BGR2GRAY)
+    feature = cv2.HuMoments(cv2.moments(image)).flatten()
+    return feature
 
-rf = RandomForestClassifier(n_estimators=100, criterion='gini' ,max_depth=30,  min_samples_leaf=2, max_features=30)
-rf = rf.fit(x_train, y_train)
+def fd_haralick(image):
+    gray = cv2.cvtColor(image, cv2.COLOR_BGR2GRAY)
+    haralick = mahotas.features.haralick(gray).mean(axis=0)
+    return haralick
 
-rf.fit(x_train, y_train)
+def fd_histogram(image, mask=None):
+    image = cv2.cvtColor(image, cv2.COLOR_BGR2HSV)
+    hist  = cv2.calcHist([image], [0, 1, 2], None, [256, 256, 256], [0, 256, 0, 256, 0, 256])
+    cv2.normalize(hist, hist)
+    return(hist.flatten())
 
-# best_params = rf.best_params_
-# print("Best Parameters:", best_params)
+feature=[]
+def dataframe(df):
 
-y_pred_train = rf.predict(x_train)
-y_pred_test = rf.predict(x_test)
+    image=cv2.imread(df['Path'])
+    global_feature = np.hstack([fd_haralick(image), fd_hu_moments(image),df['label']])
+    feature.append(global_feature)
+    
+train_data.apply(dataframe,axis=1)
 
-cm = confusion_matrix(y_test, y_pred_test)
-plt.figure(figsize=(10, 8))
-sns.heatmap(cm, annot=True, cmap='Blues', fmt='g', xticklabels=['Parasitized', 'Uninfected'], yticklabels=['Parasitized', 'Uninfected'])
-plt.xlabel('Predicted')
-plt.ylabel('Actual')
-plt.title('Confusion Matrix')
-plt.show()
+X_train=pd.DataFrame(feature).drop(columns=[20])
+y_train=train_data['label']
 
-    # training metrics
+feature=[]
+test_data.apply(dataframe,axis=1)
+
+X_test=pd.DataFrame(feature).drop(columns=[20])
+y_test=test_data['label']
+
+scaler=StandardScaler()
+scaler.fit(X_train)
+X_train=scaler.transform(X_train)
+X_test=scaler.transform(X_test)
+
+svc=SVC()
+svc.fit(X_train,y_train)
+pred=svc.predict(X_test)
+
+print(accuracy_score(y_test, pred))
+
+# Random Forest Classifier langsung menggunakan paramter terbaik
+rf = RandomForestClassifier(n_estimators=90, criterion='entropy' ,max_depth=30, min_samples_leaf=2, max_features=40)
+rf.fit(X_train, y_train)
+pred = rf.predict(X_test)
+
+y_pred_train = rf.predict(X_train)
+y_pred_test = rf.predict(X_test)
+
+print(accuracy_score(y_train, y_pred_train))
+print(accuracy_score(y_test, y_pred_test))
+
 print("Training metrics:")
 print(sklearn.metrics.classification_report(y_true= y_train, y_pred= y_pred_train))
-    
-    # test data metrics
+
 print("Test data metrics:")
 print(sklearn.metrics.classification_report(y_true= y_test, y_pred= y_pred_test))
 
+cm = confusion_matrix(y_test, pred)
+disp = ConfusionMatrixDisplay(confusion_matrix=cm, display_labels=['Uninfected', 'Parasitized'])
+
+disp.plot(cmap="Blues")
+plt.show()
 
 
-### Fungsi untuk klasifikasi gambar ###
-def test(img):
-    resize_img = cv2.resize(img, (100, 100))
-    array_image = np.array(resize_img)
-    array_image = array_image.reshape(1, -1)
+def classify_image(image_path):
+    image = cv2.imread(image_path)
+    global_feature = np.hstack([fd_haralick(image), fd_hu_moments(image)])
+    scaled_feature = scaler.transform([global_feature])
+    prediction = rf.predict(scaled_feature)
+    
+    if prediction == 0:
+        print(f"citra '{image_path}' diklasifikasi sebegai Uninfected.")
+    else:
+        print(f"citra '{image_path}' diklasifikasi sebegai Parasitized.")
+    
 
-    print(rf.predict(array_image).reshape(1, -1))
-
-    plt.imshow(array_image.reshape(100, 100, 3))    
+    plt.imshow(cv2.cvtColor(image, cv2.COLOR_BGR2RGB))
+    plt.title("Citra asli")
+    plt.axis("off")
     plt.show()
-
